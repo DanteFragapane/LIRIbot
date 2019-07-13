75521e0e3562b40364e046060e46df7da7750e0c
Dante Fragapane <dante.fragapane@gmail.com>
2019-07-13 17:54:50 -0400

    Added error handling

--- liriBonus.js
+++ liriBonus.js
@@ -2,187 +2,185 @@
 // =====================================
 
 // Read and set environment variables
-require("dotenv").config();
+require('dotenv').config()
 
 // Import the API keys
-var keys = require("./keys");
+var keys = require('./keys')
 
 // Import the node-spotify-api NPM package.
-var Spotify = require("node-spotify-api");
+var Spotify = require('node-spotify-api')
 
 // Import the axios npm package.
-var axios = require("axios");
+var axios = require('axios')
 
 // Import the moment npm package.
-var moment = require("moment");
+var moment = require('moment')
 
 // Import the FS package for read/write.
-var fs = require("fs");
+var fs = require('fs')
 
 // Initialize the spotify API client using our client id and secret
-var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify)
 
 // FUNCTIONS
 // =====================================
 
 // Writes to the log.txt file
-var writeToLog = function(data) {
+var writeToLog = function (data) {
   // Append the JSON data and add a newline character to the end of the log.txt file
-  fs.appendFile("log.txt", JSON.stringify(data) + "\n", function(err) {
+  fs.appendFile('log.txt', JSON.stringify(data) + '\n', function (err) {
     if (err) {
-      return console.log(err);
+      return console.log(err)
     }
 
-    console.log("log.txt was updated!");
-  });
-};
+    console.log('log.txt was updated!')
+  })
+}
 
 // Helper function that gets the artist name
-var getArtistNames = function(artist) {
-  return artist.name;
-};
+var getArtistNames = function (artist) {
+  return artist.name
+}
 
 // Function for running a Spotify search
-var getMeSpotify = function(songName) {
+var getMeSpotify = function (songName) {
   if (songName === undefined) {
-    songName = "What's my age again";
+    songName = "What's my age again"
   }
 
-  spotify.search({ type: "track", query: songName }, function(err, data) {
+  spotify.search({ type: 'track', query: songName }, function (err, data) {
     if (err) {
-      console.log("Error occurred: " + err);
-      return;
+      console.log('Error occurred: ' + err)
+      return
     }
 
-    var songs = data.tracks.items;
-    var data = [];
+    var songs = data.tracks.items
+    var data = []
 
     for (var i = 0; i < songs.length; i++) {
       data.push({
-        "artist(s)": songs[i].artists.map(getArtistNames),
-        "song name: ": songs[i].name,
-        "preview song: ": songs[i].preview_url,
-        "album: ": songs[i].album.name
-      });
+        'artist(s)': songs[i].artists.map(getArtistNames),
+        'song name: ': songs[i].name,
+        'preview song: ': songs[i].preview_url,
+        'album: ': songs[i].album.name
+      })
     }
 
-    console.log(data);
-    writeToLog(data);
-  });
-};
+    console.log(data)
+    writeToLog(data)
+  })
+}
 
 // Function for concert search
-var getMyBands = function(artist) {
-  var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+var getMyBands = function (artist) {
+  var queryURL = 'https://rest.bandsintown.com/artists/' + artist + '/events?app_id=codingbootcamp'
 
-  axios.get(queryURL).then(
-    function(response) {
-      var jsonData = response.data;
+  axios.get(queryURL).then(function (response) {
+    var jsonData = response.data
 
-      if (!jsonData.length) {
-        console.log("No results found for " + artist);
-        return;
-      }
-
-      var logData = [];
-
-      logData.push("Upcoming concerts for " + artist + ":");
-
-      for (var i = 0; i < jsonData.length; i++) {
-        var show = jsonData[i];
-
-        // Push each line of concert data to `logData`
-        // If a concert doesn't have a region, display the country instead
-        // Use moment to format the date
-        logData.push(
-          show.venue.city +
-            "," +
-            (show.venue.region || show.venue.country) +
-            " at " +
-            show.venue.name +
-            " " +
-            moment(show.datetime).format("MM/DD/YYYY")
-        );
-      }
-
-      // Print and write the concert data as a string joined by a newline character
-      console.log(logData.join("\n"));
-      writeToLog(logData.join("\n"));
+    if (!jsonData.length) {
+      console.log('No results found for ' + artist)
+      return
     }
-  );
-};
+
+    var logData = []
+
+    logData.push('Upcoming concerts for ' + artist + ':')
+
+    for (var i = 0; i < jsonData.length; i++) {
+      var show = jsonData[i]
+
+      // Push each line of concert data to `logData`
+      // If a concert doesn't have a region, display the country instead
+      // Use moment to format the date
+      logData.push(
+        show.venue.city +
+          ',' +
+          (show.venue.region || show.venue.country) +
+          ' at ' +
+          show.venue.name +
+          ' ' +
+          moment(show.datetime).format('MM/DD/YYYY')
+      )
+    }
+
+    // Print and write the concert data as a string joined by a newline character
+    console.log(logData.join('\n'))
+    writeToLog(logData.join('\n'))
+  })
+}
 
 // Function for running a Movie Search
-var getMeMovie = function(movieName) {
+var getMeMovie = function (movieName) {
   if (movieName === undefined) {
-    movieName = "Mr Nobody";
+    movieName = 'Mr Nobody'
   }
 
-  var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+  var urlHit = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=full&tomatoes=true&apikey=trilogy'
 
-  axios.get(urlHit).then(
-    function(response) {
-      var jsonData = response.data;
+  axios.get(urlHit).then(function (response) {
+    var jsonData = response.data
 
-      var data = {
-        "Title:": jsonData.Title,
-        "Year:": jsonData.Year,
-        "Rated:": jsonData.Rated,
-        "IMDB Rating:": jsonData.imdbRating,
-        "Country:": jsonData.Country,
-        "Language:": jsonData.Language,
-        "Plot:": jsonData.Plot,
-        "Actors:": jsonData.Actors,
-        "Rotten Tomatoes Rating:": jsonData.Ratings[1].Value
-      };
-
-      console.log(data);
-      writeToLog(data);
+    var data = {
+      'Title:': jsonData.Title,
+      'Year:': jsonData.Year,
+      'Rated:': jsonData.Rated,
+      'IMDB Rating:': jsonData.imdbRating,
+      'Country:': jsonData.Country,
+      'Language:': jsonData.Language,
+      'Plot:': jsonData.Plot,
+      'Actors:': jsonData.Actors,
+      'Rotten Tomatoes Rating:': jsonData.Ratings[1].Value
     }
-  );
-};
+
+    console.log(data)
+    writeToLog(data)
+  })
+}
 
 // Function for running a command based on text file
-var doWhatItSays = function() {
-  fs.readFile("random.txt", "utf8", function(error, data) {
-    console.log(data);
+var doWhatItSays = function () {
+  fs.readFile('random.txt', 'utf8', function (error, data) {
+    if (error) {
+      console.error(error)
+    }
+    console.log(data)
 
-    var dataArr = data.split(",");
+    var dataArr = data.split(',')
 
     if (dataArr.length === 2) {
-      pick(dataArr[0], dataArr[1]);
+      pick(dataArr[0], dataArr[1])
+    } else if (dataArr.length === 1) {
+      pick(dataArr[0])
     }
-    else if (dataArr.length === 1) {
-      pick(dataArr[0]);
-    }
-  });
-};
+  })
+}
 
 // Function for determining which command is executed
-var pick = function(caseData, functionData) {
+var pick = function (caseData, functionData) {
   switch (caseData) {
-  case "concert-this":
-    getMyBands(functionData);
-    break;
-  case "spotify-this-song":
-    getMeSpotify(functionData);
-    break;
-  case "movie-this":
-    getMeMovie(functionData);
-    break;
-  case "do-what-it-says":
-    doWhatItSays();
-    break;
-  default:
-    console.log("LIRI doesn't know that");
+    case 'concert-this':
+      getMyBands(functionData)
+      break
+    case 'spotify-this-song':
+      getMeSpotify(functionData)
+      break
+    case 'movie-this':
+      getMeMovie(functionData)
+      break
+    case 'do-what-it-says':
+      doWhatItSays()
+      break
+    default:
+      console.log("LIRI doesn't know that")
   }
-};
+}
 
 // Function which takes in command line arguments and executes correct function accordingly
-var runThis = function(argOne, argTwo) {
-  pick(argOne, argTwo);
-};
+var runThis = function (argOne, argTwo) {
+  pick(argOne, argTwo)
+}
 
 // MAIN PROCESS
 // =====================================
-runThis(process.argv[2], process.argv.slice(3).join(" "));
+runThis(process.argv[2], process.argv.slice(3).join(' '))
